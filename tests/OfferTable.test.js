ffc8e5a26596bbc324cadc2eecd46f810aac7279
Robert Kawecki <robert+github@rkaw.pl>
2017-02-13 20:51:44 +0100

    Add actual application of trades
    * The Trade type is now formalized
    * The OfferTable now applies each trade as it is added, or queues it up if not immediately applicable
    * Events are emitted upon successful trades

--- tests/OfferTable.test.js
+++ tests/OfferTable.test.js
@@ -5,6 +5,10 @@
 const Offer = require('../lib/classes/Offer');
 const OfferTable = require('../lib/classes/OfferTable');
 
+function dump(value) {
+  return JSON.stringify(value, null, '  ');
+}
+
 // Test purchase offers:
 const purchase1 = new Offer({ instrumentCode: 'ANGULAR3', type: 'purchase', price: '100.00', quantity: 5 });
 const purchase2 = new Offer({ instrumentCode: 'ANGULAR3', type: 'purchase', price: '98.00', quantity: 200 });
@@ -12,19 +16,22 @@ const purchase2 = new Offer({ instrumentCode: 'ANGULAR3', type: 'purchase', pric
 // Test sale offers:
 const sale1 = new Offer({ instrumentCode: 'ANGULAR3', type: 'sale', price: '120.00', quantity: 504 });
 const sale2 = new Offer({ instrumentCode: 'ANGULAR3', type: 'sale', price: '100.00', quantity: 4 });
-const sale3 = new Offer({ instrumentCode: 'ANGULAR3', type: 'sale', price: '90.00', quantity: 3 });
+const sale3 = new Offer({ instrumentCode: 'ANGULAR3', type: 'sale', price: '90.00', quantity: 8 });
 
+const table = new OfferTable({ instrumentCode: 'ANGULAR3' });
+
+table.on('trade', function(trade) {
+  console.log(dump(trade));
+});
 
 // The trade begins!
-const table = new OfferTable({ instrumentCode: 'ANGULAR3' });
+console.log('adding purchase1');
 table.addOffer({ offer: purchase1 });
+console.log('adding purchase2');
 table.addOffer({ offer: purchase2 });
+console.log('adding sale1');
 table.addOffer({ offer: sale1 });
+console.log('adding sale2');
 table.addOffer({ offer: sale2 });
+console.log('adding sale3');
 table.addOffer({ offer: sale3 });
-
-function dump(value) {
-  return JSON.stringify(value, null, '  ');
-}
-
-console.log(dump(table._findTrades(sale3)));
